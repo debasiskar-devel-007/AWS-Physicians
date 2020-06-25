f0a7170e20e8b90fbd6339954c30d96409d23a2c
avijit.influxiq <avijit.influxiq@gmail.com>
2020-06-25 19:14:44 +0530

    avijit

--- src/app/components/doctor-recommend/doctor-recommend.component.ts
+++ src/app/components/doctor-recommend/doctor-recommend.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor-recommend.component.css']
 })
 export class DoctorRecommendComponent implements OnInit {
+  public youtubeFlage:boolean=false;
+  public hideSpan:boolean=true;
 
   constructor() { }
 
   ngOnInit() {
   }
+ /**show video modal on click of thamnail function by sourav */
+ fetchvideo(){
+  this.youtubeFlage=true;
+  this.hideSpan=false;
+}  
 
 }
