bda2f47f4d8d5cd992c65d64ec2f52c688cb564e
avijit.influxiq <avijit.influxiq@gmail.com>
2020-07-07 20:12:40 +0530

    avijit

--- src/app/components/software-walk-through/software-walk-through.component.ts
+++ src/app/components/software-walk-through/software-walk-through.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, ViewEncapsulation, HostListener  } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material';
+export interface DialogData {
+  data: any;
+}
 @Component({
   selector: 'app-software-walk-through',
   templateUrl: './software-walk-through.component.html',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SoftwareWalkThroughComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog:MatDialog) { }
 
   ngOnInit() {
+  }
+  viewImage(val:any){
+    console.log(val)
+            //console.log('>>>>>>>>>>>>>>>>>>',this.safeSrc)
+            const dialogRef = this.dialog.open(ImageModalComponent, {
+              // panelClass:['modal-md','success-modal'],
+              panelClass: 'image_modal',
+              width:'1000px',
+              height:'450px',
+              data: val
+            });
+            dialogRef.afterClosed().subscribe(result => {
+            }); 
+    }
+
+}
+
+@Component({
+  selector: 'app-imageModal',
+  templateUrl: './imageModal.html'
+})
+export class ImageModalComponent {
+  public profile: any;
+  public fulldata:any;
+  constructor(public dialogRef: MatDialogRef<ImageModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+ 
   }
 
 }
