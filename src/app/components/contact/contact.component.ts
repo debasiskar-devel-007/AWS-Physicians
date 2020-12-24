1df7dff098ef2b741dc0a4722f193cbc7156a7a2
influxiq1 <himadribar.influxiq@gmail.com>
2020-12-24 18:11:11 +0530

    up-to-date

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+public contactusdata: any = {
+  firstname:'',
+  lastname:'',
+  email:'',
+  phone:'',
+  description:'',
+  capture:''
+};
+  constructor(public apiService: ApiService) { }
 
   ngOnInit() {
+  }
+  doSubmit(){
+    console.log(this.contactusdata,'+++++++');
+    let data = {data:this.contactusdata}
+    this.apiService.postdata(data ,'https://219s17oisi.execute-api.us-east-1.amazonaws.com/dev/api/addcontactusforhps').subscribe((res: any) => {
+      console.log(res);
+      if (res.status === 'success') {
+        console.log(res);
+        this.contactusdata = {
+          firstname:'',
+          lastname:'',
+          email:'',
+          phone:'',
+          description:'',
+          capture:''
+        }
+      }
+    });
   }
 
 }
