5685cfb674e81f38961a7c8cb27de709d97799be
Akash-influxiq <akash.influxiq@gmail.com>
2021-02-23 10:18:49 +0530

    uptodate

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,52 +2,53 @@ import { Injectable, Inject, Component } from '@angular/core';
 import { Observable, interval, pipe, throwError } from 'rxjs';
 import { switchMap, map, takeWhile, catchError } from 'rxjs/operators';
 /*import { environment } from '../../environments/environment';*/
-import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 /*import { JwtHelperService } from '@auth0/angular-jwt';
 // import { LoggedinService } from '../loggedin.service';*/
 import { CookieService } from 'ngx-cookie-service';
 import { LOCAL_STORAGE, WINDOW } from '@ng-toolkit/universal';
-import {environment} from '../environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class ApiService {
-  public nodesslurl =  environment["api_url"];
+  public nodesslurl = environment["api_url"];
   public apiurl: any = environment["landing_api_url"];
-  constructor(private _http: HttpClient,public cookie:CookieService) {}
+  public worksheet_apiurl: any = environment["api_calender_url"];
+  constructor(private _http: HttpClient, public cookie: CookieService) { }
 
-  getData(endpoint:string){
+  getData(endpoint: string) {
     var result = this._http.get(this.getEndpointUrl(endpoint)).pipe(map(res => res));
 
-		return result;
+    return result;
   } //end getData
 
-  
-  postData(endpoint:string, source, condition){
-      var result =this._http.post(this.getEndpointUrl(endpoint),{source:source,condition:condition}/*JSON.stringify(data)*/).pipe(map(res => res));
-      return result;
+
+  postData(endpoint: string, source, condition) {
+    var result = this._http.post(this.getEndpointUrl(endpoint), { source: source, condition: condition }/*JSON.stringify(data)*/).pipe(map(res => res));
+    return result;
   } //end postData
-  
-  
-  putData(endpoint:string,data,id:string,is_cache_buster=true){
-    if (is_cache_buster==true){
+
+
+  putData(endpoint: string, data, id: string, is_cache_buster = true) {
+    if (is_cache_buster == true) {
       let ran = Math.floor(Math.random() * 10000) + 1;
       var cache_buster = '?cache=' + ran.toString();
       endpoint = endpoint + cache_buster;
     }
-  
-    var result =this._http.put(this.getEndpointUrl(endpoint)+'/'+id,JSON.stringify(data)).pipe(map(res => res));
-  
+
+    var result = this._http.put(this.getEndpointUrl(endpoint) + '/' + id, JSON.stringify(data)).pipe(map(res => res));
+
     return result;
   } //end putData
- 
-  
+
+
   // deleteData(endpoint:string,id:string){
   //   var result = this._http.delete(this.getEndpointUrl(endpoint)+"/"+id).pipe(map(res => res));
-	// 	return result;
+  // 	return result;
   // } //end deleteData
 
-  private getEndpointUrl(endpoint:string){
-      return this.nodesslurl + endpoint+'?token='+this.cookie.get('jwttoken');
+  private getEndpointUrl(endpoint: string) {
+    return this.nodesslurl + endpoint + '?token=' + this.cookie.get('jwttoken');
   }
   customRequest(requestdata: any, endpoint: any) {
     // const httpOptions = {
@@ -56,14 +57,14 @@ export class ApiService {
     //     'Authorization': this.cookie.get('jwttoken')
     //   })
     // };
-    if (this.cookie.get('jwttoken') !=null && this.cookie.get('jwttoken') !='') {
-      var result = this._http.post( this.nodesslurl+endpoint+'?token='+this.cookie.get('jwttoken'), requestdata).pipe(map(res => res));
-    return result;
-    }else{
-      var result = this._http.post( this.nodesslurl+endpoint, requestdata).pipe(map(res => res));
+    if (this.cookie.get('jwttoken') != null && this.cookie.get('jwttoken') != '') {
+      var result = this._http.post(this.nodesslurl + endpoint + '?token=' + this.cookie.get('jwttoken'), requestdata).pipe(map(res => res));
+      return result;
+    } else {
+      var result = this._http.post(this.nodesslurl + endpoint, requestdata).pipe(map(res => res));
       return result;
     }
-   
+
   }
 
   postdata(requestdata: any, endpoint: any) {
@@ -73,9 +74,9 @@ export class ApiService {
     //     'Authorization': this.cookie.get('jwttoken')
     //   })
     // };
-      var result = this._http.post( endpoint, requestdata).pipe(map(res => res));
+    var result = this._http.post(endpoint, requestdata).pipe(map(res => res));
     return result;
-   
+
   }
   getState() {
     // const httpOptions = {
@@ -88,7 +89,7 @@ export class ApiService {
       return throwError(error);
     }), map(response => response));
     return result;
-  
+
   }
 
   getCity() {
@@ -97,24 +98,40 @@ export class ApiService {
       return throwError(error);
     }), map(response => response));
     return result;
-  
+
   }
 
-  getDatalistForSubmit(endpoint:any ,requestdata: any) {
+  getDatalistForSubmit(endpoint: any, requestdata: any) {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': this.cookie.get('jwtToken')
       })
     };
-     //console.log(requestdata.api_url)
-    
-      var result = this._http.post(this.apiurl + endpoint, requestdata, httpOptions).pipe(map(res => res));
-      return result;
-    
+    //console.log(requestdata.api_url)
 
-  
-  
+    var result = this._http.post(this.apiurl + endpoint, requestdata, httpOptions).pipe(map(res => res));
+    return result;
+
+
+
+
+  }
+  customRequest1(endpoint: any, requestdata: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': this.cookie.get('jwtToken')
+      })
+    };
+    //console.log(requestdata.api_url)
+
+    var result = this._http.post(this.worksheet_apiurl + endpoint, requestdata, httpOptions).pipe(map(res => res));
+    return result;
+
+
+
+
   }
 
 
--- src/app/components/landingpage/landingpage.component.ts
+++ src/app/components/landingpage/landingpage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 import { ActivatedRoute } from '@angular/router';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, ErrorStateMatcher } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, ErrorStateMatcher, MatSnackBar } from '@angular/material';
 import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 
 // export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -16,6 +16,8 @@ import { FormBuilder, FormGroup, Validators, FormControl, FormGroupDirective, Ng
 export interface DialogData {
   msg: string;
   flag: string;
+  userid: any;
+  product: any
 }
 @Component({
   selector: 'app-landingpage',
@@ -23,295 +25,153 @@ export interface DialogData {
   styleUrls: ['./landingpage.component.css']
 })
 export class LandingpageComponent implements OnInit {
-  public formdata: any ;
-  public cityVal:any = [];
-  public stateVal:any = [];
+  public formdata: any;
+  public cityVal: any = [];
+  public stateVal: any = [];
   options: FormGroup;
   //matcher = new MyErrorStateMatcher();
   // public emailregex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   //  public passwordregex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
-  constructor(public apiservice: ApiService,private activatedroute: ActivatedRoute,public dialog: MatDialog,public fb:FormBuilder) { 
+  constructor(public apiservice: ApiService, private activatedroute: ActivatedRoute, public dialog: MatDialog, public fb: FormBuilder, public route: ActivatedRoute) {
     this.options = this.fb.group({
-      firstname:['', Validators.required],
-      lastname:['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
-      phone:['',Validators.required],
-      companyname:['',Validators.required],
+      phone: ['', Validators.required],
+      companyname: ['', Validators.required],
       city: ['', Validators.required],
       state: ['', Validators.required],
       zipcode: ['', Validators.required],
     })
-    
+
     this.apiservice.getCity().subscribe((response: any) => {
       for (const i in response) {
         this.cityVal.push(
           { 'val': response[i].city, 'name': response[i].city },
         );
       }
-  
-  });
 
-  this.apiservice.getState().subscribe((response: any) => {
-    for (const i in response) {
-      this.stateVal.push(
-        { 'val': response[i].abbreviation, 'name': response[i].name },
-      );
-    }
-  
-  });
+    });
 
-    // this.formdata = {
-    //   successmessage: 'Added Successfully !!',
-    //   submittext: 'Submit',
-    //   canceltext: 'Cancel',
-    //   resettext: 'Reset',
-    //   //redirectpath: this.redirceturl,
-    //   submitactive: true, 
-    // //  apiUrl: this._apiService.api_url,
-    //  // endpoint: 'api1/resourceadd',
-    //  // jwttoken: this._apiService.jwtToken,
-    //   //cancelroute: this.redirceturl,
-    //   hidereset:true,
-    //   hidecancel:true,
-    //   fields: [
-    //     {
-    //       label: 'First Name',
-    //       name: 'firstname',
-    //       type: 'text',
-    //       validations: [
-    //         { rule: 'required', message: "First Name field is required" },
-    //     ]
-    //     },
-    //     {
-    //       label: 'Last Name',
-    //       name: 'lastname',
-    //       type: 'text',
-    //       validations: [
-    //         { rule: 'required', message: "Last Name field is required" },
-    //     ]
-    //     },
-    //     {
-    //       label: 'Phone No',
-    //       name: 'phone',
-    //       type: 'number',
-    //       validations: [
-    //         { rule: 'required', message: "Phone No field is required" },
-    //     ]
-    //     },
-    //     {
-    //       label: "Email",
-    //       name: "email",
-    //       type: 'email',
-    //       validations: [
-    //           { rule: 'required', message: "Email field is required" },
-    //           { rule: 'pattern', value: this.emailregex, message: "Must be a valid Email" }
-    //       ]
-          
-    //   },
-    //   {
-    //     label: "Password",
-    //     name: "password",
-    //     type: 'password',
-    //     value: '',
-    //     validations: [
-    //         { rule: 'required', message: "Password field is required" },
-    //         { rule: 'pattern', value: this.passwordregex, message: "Must contain a Capital Letter and a Number" }
-    //     ]
-    // },
-    // {
-    //     label: "Confirm Password",
-    //     name: "confirmpassword",
-    //     type: 'password',
-    //     validations: [
-    //         { rule: 'required', message: "Confirm Password field is required" },
-    //         { rule: 'match', message: "Confirm Password field Needs to  match Password" },
-    //         //{rule:'pattern',value: this.passwordregex,message: "Must contain a Capital Letter and a Number"}
-    //     ],
- 
+    this.apiservice.getState().subscribe((response: any) => {
+      for (const i in response) {
+        this.stateVal.push(
+          { 'val': response[i].abbreviation, 'name': response[i].name },
+        );
+      }
 
-    // },
+    });
 
-    // {
-    //   label: 'Company Name',
-    //   name: 'company',
-    //   type: 'text',
-    //   validations: [
-    //     { rule: 'required', message: "Company Name field is required" },
-    // ]
-    // },
-    // {
-    //   label: 'Address',
-    //   name: 'address',
-    //   type: 'textarea',
-    //   validations: [
-    //     { rule: 'required', message: "Address field is required" },
-    // ]
-    // },
-    // {
-    //   label: 'State',
-    //   name: 'state',
-    //   type: 'select',
-    //   val:this.stateVal,
-    //   validations: [
-    //     { rule: 'required', message: "State field is required" },
-    // ]
-    // },
-    // {
-    //   label: 'City',
-    //   name: 'city',
-    //   type: 'select',
-    //   val:this.cityVal,
-    //   validations: [
-    //     { rule: 'required', message: "City field is required" },
-    // ]
-    // },
-    // {
-    //   label: 'Zip',
-    //   name: 'zip',
-    //   type: 'number',
-    //   validations: [
-    //     { rule: 'required', message: "Zip field is required" },
-    // ]
-    // },
-    // {
-    //   label: 'Status',
-    //   name: 'status',
-    //   type: 'hideen',
-    //   value:1
-    // },
-        
-    //     {
-    //       label: 'Parent Id',
-    //       name: 'parentid',
-    //       type: 'hideen',
-    //       value: this.activatedroute.snapshot.params.userid
-    //     },
-    //     {
-    //       label: 'Product Id',
-    //       name: 'product_id',
-    //       type: 'hideen',
-    //       value: this.activatedroute.snapshot.params.productid
-    //     },
-    //     {
-    //       label: 'Landing Page',
-    //       name: 'landing_page',
-    //       type: 'hideen',
-    //       value:'Hps-landing-page-1'
-    //     },
-           
-        
-    //   ]
-    // };
-  
+
   }
 
   ngOnInit() {
   }
-//   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
-//   console.log('dsfdsf',group);
-//   let pass = group.controls.password.value;
-//   let confirmPass = group.controls.confirmpassword.value;
+  //   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
+  //   console.log('dsfdsf',group);
+  //   let pass = group.controls.password.value;
+  //   let confirmPass = group.controls.confirmpassword.value;
 
-//   return pass === confirmPass ? null : { notSame: true }
-// }
+  //   return pass === confirmPass ? null : { notSame: true }
+  // }
 
-inputUntouched(val: any) {
-  this.options.controls[val].markAsUntouched();
-}
+  inputUntouched(val: any) {
+    this.options.controls[val].markAsUntouched();
+  }
 
-contactUs(){     
-  // console.log('++++ not valid',this.options.value)
-   let formCalData: any;
-  for (formCalData in this.options.controls) {
-     this.options.controls[formCalData].markAsTouched();
-   }
-  if (this.options.valid) {
-    
-   let fromData ={
-     data:{
-       firstname :this.options.value.firstname,
-       lastname: this.options.value.lastname,
-       email:this.options.value.email,
-       phone:this.options.value.phone.toString(),
-       //password:this.options.value.password,
-       company:this.options.value.companyname,
-       state:this.options.value.state,
-       city:this.options.value.city,
-       zip:this.options.value.zipcode,
-       source:'Hps-landing-page-1',
-       parentid:this.activatedroute.snapshot.params.userid,
-       type:'lead',
-       status:1,
-       product_id:this.activatedroute.snapshot.params.productid
-     }
-    
-   }
+  contactUs() {
+    // console.log('++++ not valid',this.options.value)
+    let formCalData: any;
+    for (formCalData in this.options.controls) {
+      this.options.controls[formCalData].markAsTouched();
+    }
+    if (this.options.valid) {
 
-   this.apiservice.getDatalistForSubmit('api3/usersignup', fromData).subscribe((response: any) => {
-     if(response.status == "success"){
+      let fromData = {
+        data: {
+          firstname: this.options.value.firstname,
+          lastname: this.options.value.lastname,
+          email: this.options.value.email,
+          phone: this.options.value.phone.toString(),
+          //password:this.options.value.password,
+          company: this.options.value.companyname,
+          state: this.options.value.state,
+          city: this.options.value.city,
+          zip: this.options.value.zipcode,
+          source: 'Hps-landing-page-1',
+          parentid: this.activatedroute.snapshot.params.userid,
+          type: 'lead',
+          status: 1,
+          product_id: this.activatedroute.snapshot.params.productid
+        }
 
-          const dialogRef = this.dialog.open(FormConfirmComponent,{
-            panelClass:'successModal',
-            data:{ flag:'success'}
+      }
+
+      this.apiservice.getDatalistForSubmit('api3/usersignup', fromData).subscribe((response: any) => {
+        if (response.status == "success") {
+
+          const dialogRef = this.dialog.open(FormConfirmComponent, {
+            panelClass: 'successModal',
+            data: { flag: 'success', userid: this.route.snapshot.params.userid, product: this.route.snapshot.params.productid }
           });
           dialogRef.afterClosed().subscribe(result => {
           });
-        this.options.reset();
-          
-       
-     }
+          this.options.reset();
 
-     if(response.status == "error"){
 
-      const dialogRef = this.dialog.open(FormConfirmComponent,{
-        panelClass:'successModal',
-        data:{msg:response.errormessage , flag:'error'}
+        }
+
+        if (response.status == "error") {
+
+          const dialogRef = this.dialog.open(FormConfirmComponent, {
+            panelClass: 'successModal',
+            data: { msg: response.errormessage, flag: 'error' }
+          });
+          dialogRef.afterClosed().subscribe(result => {
+          });
+          // this.options.reset();
+        }
+
+      }, error => {
       });
-      dialogRef.afterClosed().subscribe(result => {
-      });
-   // this.options.reset();
-     }
-     
-    }, error => {
-    });
-    
+
+    }
   }
- }
 
 
-//   listenFormFieldChange(val:any){
-//   //console.log('ddd',val);
-//   if(val.field == "fromsubmit" && val.fieldval == "success"){
-   
-//    let submitData ={
-//      data:val.fromval
-     
-//    }
-//   // console.log('lllllllll',submitData);
-//    this.apiservice.getDatalistForSubmit('api/addusers', submitData).subscribe((response: any) => {
-//     if(response.status == "success"){
- 
-//       const dialogRef = this.dialog.open(FormConfirmComponent,{
-//         panelClass:'successModal',
-//         data:{ flag:'success'}
-//       });
-//       dialogRef.afterClosed().subscribe(result => {
-//       });
-//  }
+  //   listenFormFieldChange(val:any){
+  //   //console.log('ddd',val);
+  //   if(val.field == "fromsubmit" && val.fieldval == "success"){
 
-//  if(response.status == "error"){
+  //    let submitData ={
+  //      data:val.fromval
 
-//   const dialogRef = this.dialog.open(FormConfirmComponent,{
-//     panelClass:'successModal',
-//     data:{msg:response.errormessage , flag:'error'}
-//   });
-//   dialogRef.afterClosed().subscribe(result => {
-//   });
-//  }
-//    });
-//   }
+  //    }
+  //   // console.log('lllllllll',submitData);
+  //    this.apiservice.getDatalistForSubmit('api/addusers', submitData).subscribe((response: any) => {
+  //     if(response.status == "success"){
 
-//   }
+  //       const dialogRef = this.dialog.open(FormConfirmComponent,{
+  //         panelClass:'successModal',
+  //         data:{ flag:'success'}
+  //       });
+  //       dialogRef.afterClosed().subscribe(result => {
+  //       });
+  //  }
+
+  //  if(response.status == "error"){
+
+  //   const dialogRef = this.dialog.open(FormConfirmComponent,{
+  //     panelClass:'successModal',
+  //     data:{msg:response.errormessage , flag:'error'}
+  //   });
+  //   dialogRef.afterClosed().subscribe(result => {
+  //   });
+  //  }
+  //    });
+  //   }
+
+  //   }
 }
 
 @Component({
@@ -321,8 +181,107 @@ contactUs(){
 })
 
 export class FormConfirmComponent {
+  public formdata: any = {};
+  public progressSpinner: any = {
+    mode: 'indeterminate',
+    loading: false,
+    bookingStatus: 'Sending request'
+  };
+  public productid: any;
+
   constructor(public dialogRef: MatDialogRef<FormConfirmComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData){
-   // console.log('ddd',data)
+    @Inject(MAT_DIALOG_DATA) public data: DialogData, public apiservice: ApiService, public snackBar: MatSnackBar) {
+    console.log(data.product, 'form+++++++++++++++==')
+    this.productid = data.product
+    // this.formdata = {
+    //   successmessage: 'Submitted Successfully !!',
+    //   submitactive: true, // optional, default true
+    //   submittext: 'Submit',
+    //   jwttoken: '',
+    //   // cancelroute: '',
+    //   // custombuttons: [
+    //   //   {
+    //   //     name: 'save',
+    //   //     label: 'Save',
+    //   //     class: 'savecls',
+    //   //     type: 'button'
+    //   //   }
+    //   // ],
+    //   fields: [
+    //     {
+    //       label: 'Projects all cost differently, but we require a minimum of $100k on all potential custom software builds.  Can your potential client afford this?',
+    //       name: 'cost',
+    //       value: '',
+    //       type: 'text',
+    //       validations: [
+    //         { rule: 'required', message: 'field is required' },
+    //       ]
+    //     },
+    //     {
+    //       label: 'Is the person getting on the call able to make a financial decision for the company?',
+    //       name: 'company_financial',
+    //       value: 1,
+    //       type: 'radio',
+    //       validations: [
+    //         { rule: 'required', message: 'field is required' },
+    //       ],
+    //       val: [{ key: 1, val: 'Yes' }]
+    //     },
+    //     {
+    //       label: 'Give a brief description of what they are looking for: ',
+    //       name: 'description',
+    //       value: '',
+    //       type: 'textarea',
+    //       validations: [
+    //         { rule: 'required', message: 'field is required' },
+    //       ]
+    //     }
+    //   ]
+    // }
+    // console.log('ddd',data)
   }
- }
+
+  // listenFormFieldChange(val: any) {
+  //   console.log(val, '++=++')
+
+
+
+  //   //for submit
+
+
+
+  //   if (val.field == 'fromsubmit' && val.fieldval == 'success') {
+  //     this.progressSpinner.loading = true;
+
+  //     // this.data.flag = 'submit';
+  //     // this.data.val = val;
+  //     // this.data.worksheet_val = 1;
+
+
+  //     let form_data = {
+  //       worksheet_data: val.fromval,
+  //       worksheet_val: 1,
+  //       user_id: this.data.userid,
+  //       product_id: this.data.product
+  //     }
+
+  //     this.apiservice.customRequest1('work-sheet', form_data).subscribe((response: any) => {
+  //       if (response.status == 'success') {
+  //         this.progressSpinner.loading = false;
+
+  //         this.openSnackBar('Worksheet Submit Successfully.', null);
+
+  //         this.dialogRef.close(this.data);
+
+  //       } else {
+  //         this.openSnackBar('Something went wrong. Please try again.', null);
+  //       }
+  //     })
+  //   }
+  // }
+  openSnackBar(message: string, action: string = null) {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+    });
+  }
+}
