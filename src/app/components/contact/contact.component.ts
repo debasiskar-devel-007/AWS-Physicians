c867b23bd1d034f0b5b9581e4def5cfe18298366
Abirlal Mukherjee <abirlal.influxiq@gmail.com>
2021-03-01 13:48:47 +0530

    Modification in the contact us

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -13,6 +13,7 @@ import {MatSnackBar} from '@angular/material';
 export class ContactComponent implements OnInit {
 public code:any = '';
 public errorCaptchaMessage:any = true
+public clickCaptcha:boolean = false
 options: FormGroup;
 public contactusdata: any = {
   first_name:'',
@@ -35,23 +36,26 @@ public contactusdata: any = {
       // twitter_card:environment.share_image,
     };
     this.metaservice.setmeta(data);
+  
     this.options = this.fb.group({
-      first_name:['', [Validators.required,Validators.minLength(3)]],
-      last_name:['', [Validators.required,Validators.minLength(3)]],
+      first_name:['', Validators.required],
+      last_name:['', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
       phone:['',Validators.required],
       message:['',Validators.required],
       captcha:['',Validators.required]
     })    
-
+    this.clickCaptcha = false
     console.log("sssss======>",this.options .controls)
+    console.log("Click event const ===>",this.clickCaptcha)
 
    }
 
-  ngOnInit() {
-    this.createCaptcha()
+  ngOnInit() {    
+    this.createCaptchaOnload()
   }
   doSubmit(){
+    console.log("Click event ===>",this.clickCaptcha)
     let formCalData: any;
     for (formCalData in this.options.controls) {
        this.options.controls[formCalData].markAsTouched();
@@ -60,7 +64,8 @@ public contactusdata: any = {
     if (this.options.valid) {    
       console.log(this.contactusdata,'+++++++');
       let captchadata = this.contactusdata.captcha;
-      this.errorCaptchaMessage = (this.code === userGivenCaptcha) ? true : false;
+      
+      this.errorCaptchaMessage = (userGivenCaptcha!='' && (this.code === userGivenCaptcha)) ? true : false;
       console.log(this.errorCaptchaMessage);
       if(this.errorCaptchaMessage){
         let data = {data:this.contactusdata}
@@ -86,8 +91,13 @@ public contactusdata: any = {
     }
   }
  
-  createCaptcha() {
+
+  createCaptcha($event) {
+      $event.preventDefault()
+    
+
     //clear the contents of captcha div first 
+    console.log("Click event ===>",this.clickCaptcha)
     var charsArray =
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
     var lengthOtp = 6;
@@ -108,8 +118,36 @@ public contactusdata: any = {
     ctx.strokeText(captcha.join(""), 0, 30);
     //storing captcha so that can validate you can save it somewhere else according to your specific requirements
     this.code = captcha.join("");
+    
+    
     return this.code
     //document.getElementById("captcha").appendChild(canv); // adds the canvas to the body element
+  }
+
+  createCaptchaOnload(){
+    //clear the contents of captcha div first 
+    console.log("Click event ===>",this.clickCaptcha)
+    var charsArray =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+    var lengthOtp = 6;
+    var captcha = [];
+    for (var i = 0; i < lengthOtp; i++) {
+      //below code will not allow Repetition of Characters
+      var index = Math.floor(Math.random() * charsArray.length + 1); //get the next character from the array
+      if (captcha.indexOf(charsArray[index]) == -1)
+        captcha.push(charsArray[index]);
+      else i--;
+    }
+    var canv = document.createElement("canvas");
+    canv.id = "captcha";
+    canv.width = 100;
+    canv.height = 50;
+    var ctx = canv.getContext("2d");
+    ctx.font = "25px Georgia";
+    ctx.strokeText(captcha.join(""), 0, 30);
+    //storing captcha so that can validate you can save it somewhere else according to your specific requirements
+    this.code = captcha.join("");    
+    return this.code
   }
 
   inputUntouched(val: any) {
