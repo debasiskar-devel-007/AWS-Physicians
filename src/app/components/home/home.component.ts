62e3e3c1ef2f547a6ff742a0d2d1df40502ee56e
Avijit95 <avijit.influxiq@gmail.com>
2021-03-22 13:01:00 +0530

    avijit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit {
       og_description:'PECETM - The Complete ANS Testing Medical Device Platform for Physicians and their practice. This cutting-edge technology offers better patient data to significantly improve patient outcomes.',
       og_url: 'https://healthprofitsolutions.com/',
       og_type: 'website',
-      // og_image: environment.share_image,
+      og_image: 'https://all-frontend-assets.s3.amazonaws.com/AWS-Physicians/images/HPS_OG+Image_Somnath_02-01-2021.jpg',
       // twitter_card:environment.share_image,
     };
     this.metaservice.setmeta(data);
